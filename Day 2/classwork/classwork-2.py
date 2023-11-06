75a2c80159feeccaa172c832c9df9cfde1837602
beridzezura14 <zuraberidze12@gmail.com>
2023-11-06 10:41:12 +0400

    commit

---
+++ Day 2/classwork/classwork-2.py
@@ -0,0 +1,52 @@
+# print("this is second lesson")
+
+player_hp = 100
+
+player_mana = 80
+
+spell_mana = 20
+
+print (player_hp - spell_mana)
+
+
+# class exercise ---------
+
+# შექმენით ოჯახის წევრების ასაკებისგან ცვლადებ
+
+# დაპრინტეტ ასაკები 5 წლის შემდეგ
+
+dad_age = 56
+mom_age = 51
+sister_age = 31
+
+dad_age += 5
+mom_age += 5
+sister_age += 5
+
+# print(dad_age + 5)
+# print(mom_age + 5)
+# print(sister_age + 5)
+
+print(dad_age)
+print(mom_age)
+print(sister_age)
+
+
+# math operations
+
+x = 12
+y = 6
+
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+
+print(x//y) # წერტილის შემდეგ რიცხვს ჭრის მაგ: 2.0 დაწერს 2ს. 3.6 დაწერს 3ს ა.შ
+
+
+point = 3 + 2
+print (point)
+
+point = 3 * 15
+print (point)
---
+++ Day 2/homework/homework-2.py
@@ -0,0 +1,43 @@
+score = 100 # When we write number , we dont need quotation marks
+print(score)
+
+# --------------------------
+
+# always use corresponding values for exemple: weathe describe - 
+
+temperature = 14
+print(temperature)
+
+# --------------------------
+
+# city describe
+
+city = "Tbilisi"  # When we write string type volues, we need quotation marks
+print(city)
+
+
+# --------------------------
+
+player = "James"
+score = 55
+level = 14
+
+print(player)
+print(score)
+
+
+# --------------------------
+
+band = "Beatles"
+song = "Yesterday"
+song = "Let it be"
+print(song)   #prints only last describe songs "let it be"
+
+
+# -------------------------
+
+
+name = "zura"
+
+# name - variable name
+# "zura" - variable value
